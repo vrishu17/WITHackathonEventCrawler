63153d630b1dfe039422ef5105326ce21713d071
Vrushali <vrishu17@gmail.com>
2023-02-12 15:06:47 +0530

    Added wrapper code and constats

--- app.py
+++ app.py
@@ -1,25 +1,21 @@
 from flask import Flask, render_template
-import MeetupTech
+import AllEvents
 
 app = Flask(__name__, template_folder='uitemplates',static_folder='uitemplates')
 
 @app.route("/")
 def index():
-    tech_events = MeetupTech.get_all_meetups()
+    tech_events = AllEvents.get_all()
     return render_template("index.html", events=tech_events)
 
 @app.route("/tech")
 def tech():
-    tech_events = MeetupTech.get_all_meetups()
+    tech_events = AllEvents.get_all_techs()
     return render_template("tech.html", events=tech_events)
 
 @app.route("/sports")
 def sports():
-    sports_events = [
-        {"name": "World Cup 2022", "date": "2022-06-01"},
-        {"name": "Summer Olympics 2023", "date": "2023-08-01"},
-        {"name": "Super Bowl 2024", "date": "2024-02-01"},
-    ]
+    sports_events = AllEvents.get_all_sports()
     return render_template("sports.html", events=sports_events)
 
 if __name__ == "__main__":
