26fdc4b830825a11757a719d0dd5e16159b92549
Sarah-Shahi <132733267+Sarah-Shahi@users.noreply.github.com>
2023-05-06 10:42:21 +0530

    Comments added + variable correction

--- app.py
+++ app.py
@@ -3,16 +3,19 @@ import AllEvents
 
 app = Flask(__name__, template_folder='uitemplates',static_folder='uitemplates')
 
+# API for all events
 @app.route("/")
 def index():
-    tech_events = AllEvents.get_all()
-    return render_template("index.html", events=tech_events)
+    all_events = AllEvents.get_all()
+    return render_template("index.html", events=all_events)
 
+# API for tech events
 @app.route("/tech")
 def tech():
     tech_events = AllEvents.get_all_techs()
     return render_template("tech.html", events=tech_events)
 
+# API for sports events
 @app.route("/sports")
 def sports():
     sports_events = AllEvents.get_all_sports()
